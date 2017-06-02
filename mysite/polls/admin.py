2d8cd35d87c8fc92a706a9e36b055a669faec0eb
Jack Wang <jackw@splunk.com>
2017-06-02 15:36:39 +0800

    add django tutorial project

---
+++ mysite/polls/admin.py
@@ -0,0 +1,40 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Question, Choice
+
+# admin.site.register(Question)
+
+# class QuestionAdmin(admin.ModelAdmin):
+#     fields = ['pub_date', 'question_text']
+
+# admin.site.register(Choice)
+
+
+
+# class QuestionAdmin(admin.ModelAdmin):
+#     fieldsets = [
+#         (None,               {'fields': ['question_text']}),
+#         ('Date information', {'fields': ['pub_date']}),
+#     ]
+
+
+# admin.site.register(Question, QuestionAdmin)
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ['question_text']
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
